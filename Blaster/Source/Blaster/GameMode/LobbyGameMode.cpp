543a5c6c79060e18fe012abd6fa526139791ad19
Will Brewer <willbrewer182@gmail.com>
2025-08-22 13:52:35 -0400

    Copy directory from bitbucket to github

---
+++ Blaster/Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	// WBREWER TODO: Maybe use AGameMode::NumPlayers here?
+	int32 NumConnectedPlayers = GameState.Get()->PlayerArray.Num();
+	if (NumPlayers == 2)
+	{
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Maps/BlasterMap?listen"));
+		}
+	}
+}
+
